89f5d6d3f822544a73e50657f5f8b330a136aa68
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-07-21 00:22:58 +0200

    created providers for auth and supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Figtree } from 'next/font/google';
 import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -22,7 +23,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SupabaseProvider>
-          <Sidebar>{children}</Sidebar>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
