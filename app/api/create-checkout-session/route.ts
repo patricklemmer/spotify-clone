14abf9a08bdb87bdcb7b561e0afb4e0b2a856577
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-07-27 17:24:13 +0200

    fix: url typo

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: Request) {
         metadata,
       },
       success_url: `${getUrl()}/account`,
-      cancel_url: `${getUrl()}`,
+      cancel_url: `${getUrl()}/`,
     });
 
     return NextResponse.json({ sessionId: session.id });
