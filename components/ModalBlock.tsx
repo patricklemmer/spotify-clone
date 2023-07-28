57b98677ae9f523fff3231ff0a26180f39fff33d
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-07-28 12:45:25 +0200

    updated context UI component

--- components/ModalBlock.tsx
+++ components/ModalBlock.tsx
@@ -28,7 +28,7 @@ const ModalBlock = () => {
       >
         <Modal.Header>
           <Text id="modal-title" size={18} css={{ color: '#d4d4d4' }}>
-            Payment Info <span className="font-bold">Stripe</span>
+            Sign-up and Payment Info <span className="font-bold">Stripe</span>
           </Text>
         </Modal.Header>
         <Modal.Body>
@@ -60,6 +60,16 @@ const ModalBlock = () => {
               XXX (Any three digits)
             </Text>
           </Row>
+          <p>
+            Open this link to grab a temporary email for sign up:{' '}
+            <a
+              href="https://tempmailo.com/"
+              target="_blank"
+              className="font-semibold"
+            >
+              https://tempmailo.com/
+            </a>
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button auto color="error" ghost onPress={closeHandler}>
